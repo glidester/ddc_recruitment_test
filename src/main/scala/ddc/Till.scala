35e1be813a0e980cc96b34175007e978d3a0dfca
Tim Wilkins <glidester@gmail.com>
2019-10-23 12:41:57 +0100

    Add missing comments

--- src/main/scala/ddc/Till.scala
+++ src/main/scala/ddc/Till.scala
@@ -4,6 +4,13 @@ package ddc
   * Represents the entity responsible for calculating a bill
   */
 object Till {
+
+  /**
+    * Sums all the menu items in the given list of items.
+    *
+    * @param items the menu items to be summed
+    * @return the total price of all the given menu items in pence
+    */
   def sumMenuItems(items: List[MenuItem]): Money = {
     items.foldLeft(0) ( (total, item) => total + item.price)
   }
--- src/main/scala/ddc/package.scala
+++ src/main/scala/ddc/package.scala
@@ -2,5 +2,7 @@
   * Holds the common type definitions
   */
 package object ddc {
+
+  /** Money represented as a pence value (100 == £1) */
   type Money = Int
 }
